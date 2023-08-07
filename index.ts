5e6a5c3a390b247ef9dc56913a0c2b23f7225c23
davidhe <davidhe88@pm.me>
2023-08-07 18:36:06 +0100

    why u no working?

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import Web3 from "web3";
+import { EVM } from "@dequanto/evm/EVM";
+
+const provider =
+  "https://eth-mainnet.g.alchemy.com/v2/kaP73g41gnaTbv615EkSN5Q8ZHNFTDSj";
+
+// cryptokitties
+const contractAddress = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d";
+
+const web3 = new Web3(provider);
+
+async function run() {
+  try {
+    const bytecode = await web3.eth.getCode(contractAddress);
+    const evm = new EVM(bytecode);
+    const abi = evm.getAbi();
+
+    console.log("\n Why you no working????", abi);
+  } catch (error) {
+    console.error("\n Error:", error);
+  }
+}
+
+run();
